3a4247ba0e2c2d47ae55cc5fe8ba55771982efd5
unknown <akuate@ufipay.net>
2020-11-24 14:20:37 +0100

    first commit

---
+++ src/test/java/com/org/id/registerservice/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.id.registerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
